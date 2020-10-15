36ca95788ec0ec14eec24b09e0bcca164db5ce53
sfoo0011 <sfoo0011@student.monash.edu>
2020-10-15 16:18:27 +0800

    Added initial design of stegosaur

--- src/game/Stegosaur.java
+++ src/game/Stegosaur.java
@@ -15,6 +15,11 @@ import edu.monash.fit2099.engine.GameMap;
 public class Stegosaur extends Actor {
 	// Will need to change this to a collection if Stegosaur gets additional Behaviours.
 	private Behaviour behaviour;
+	private int age = 0 ; 
+	private int foodLevel ; 
+	private int starvationLevel = 0 ; 
+	private String lifeStage ;
+	private char gender ;   
 
 	/** 
 	 * Constructor.
@@ -22,11 +27,81 @@ public class Stegosaur extends Actor {
 	 * 
 	 * @param name the name of this Stegosaur
 	 */
-	public Stegosaur(String name) {
+	public Stegosaur(String name , String lifeStage) {
 		super(name, 'd', 100);
 		
 		behaviour = new WanderBehaviour();
+		
+		if(lifeStage == "baby" || lifeStage == "adult") {
+			this.lifeStage = lifeStage;
+			if(lifeStage == "adult"){
+				this.foodLevel = 50 ; 
+				this.displayChar = 'D' ;
+			}else {
+				//baby stegosaur 
+				this.foodLevel = 10;
+			}
+		}
+		
+		if(Math.random() >0.5) {
+			this.gender = 'm' ; // set gender as male ; 
+		}else {
+			this.gender = 'f' ; // set gender as female
+		}
+
 	}
+	
+	public void Starving() {
+		this.starvationLevel +=1 ; 
+	
+	}
+	
+	public void raiseFoodLevel(int x){
+		if(this.foodLevel+x<=100) {
+			this.foodLevel += x ; 
+		}else {
+			this.foodLevel = 100 ; 
+		}
+	}
+	
+	
+	public void grazeGrass(Grass grass) {
+		raiseFoodLevel(5) ; 
+		
+	}
+	
+	public void eatHay(Hay hay) {
+		raiseFoodLevel(20) ; 
+		
+	}
+	
+	public void eatFruit(Fruit fruit) {
+		raiseFoodLevel(30) ; 
+	}
+	
+	public void hungryStegosaur(Allosaur allosaur) {
+		if(this.foodLevel < 50) {
+			//behaviour = new HungryBehaviour() ; 
+		}
+	}
+	
+	public void updateStegosaurState() {
+		this.age ++ ; 
+		
+		if(this.foodLevel >0) {
+			this.foodLevel -- ;
+		}
+	
+		if(this.foodLevel == 0) {
+			this.behaviour = null ; 
+			this.Starving();
+		}else {
+			this.starvationLevel = 0 ; 
+		}
+	}
+	
+	
+	
 
 	@Override
 	public Actions getAllowableActions(Actor otherActor, String direction, GameMap map) {
@@ -43,6 +118,12 @@ public class Stegosaur extends Actor {
 	 */
 	@Override
 	public Action playTurn(Actions actions, Action lastAction, GameMap map, Display display) {
+		
+		updateStegosaurState() ; //update stegosaur state
+		if(this.starvationLevel == 20) {
+			map.removeActor(this); // stegosaur die
+		}
+		
 		Action wander = behaviour.getAction(this, map);
 		if (wander != null)
 			return wander;
