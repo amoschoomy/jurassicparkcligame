f6fec30acf76887ab3ad82ddf989726b3c40cc74
sfoo0011 <sfoo0011@student.monash.edu>
2020-10-15 23:43:56 +0800

    Updated class HungryBehaviour

--- src/game/HungryBehaviour.java
+++ src/game/HungryBehaviour.java
@@ -8,6 +8,57 @@ public class HungryBehaviour extends Action implements Behaviour {
 	public HungryBehaviour(Actor subject) {
 		this.target = subject;
 	}
+	
+	public boolean isFruitAround(Actor actor, GameMap map) {
+		Location currentPosition = map.locationOf(actor); 
+		int closeFruit = 0 ; 
+		for(Exit exit: currentPosition.getExits()) {
+			Location destination = exit.getDestination() ; 
+			if(destination.canActorEnter(actor)) {
+				if(destination.getItems() instanceof Fruit) {
+					closeFruit++ ; 
+				} 
+			}
+					
+		}
+	
+		return closeFruit>0 ;
+	}
+	
+	public boolean isGrassAround(Actor actor, GameMap map) {
+		Location currentPosition = map.locationOf(actor); 
+		int closeGrass = 0 ; 
+		for(Exit exit: currentPosition.getExits()) {
+			Location destination = exit.getDestination() ; 
+			if(destination.canActorEnter(actor)) {
+				if(destination.getGround() instanceof Grass) {
+					closeGrass++ ; 
+				} 
+			}
+					
+		}
+	
+		return closeGrass>0 ;
+	}
+	
+	public boolean isDinosaurCorpseAround(Actor actor, GameMap map) {
+		Location currentPosition = map.locationOf(actor); 
+		int closeDino = 0 ; 
+		for(Exit exit: currentPosition.getExits()) {
+			Location destination = exit.getDestination() ; 
+			if(destination.canActorEnter(actor)) {
+				for(Item item: destination.getItems()) {
+					if(item instanceof Corpse) {
+						closeDino++; 
+					}
+				}
+			}
+					
+		}
+	
+		return closeDino>0 ;
+	}
+	
 
 	@Override
 	public String execute(Actor actor, GameMap map) {
@@ -16,13 +67,72 @@ public class HungryBehaviour extends Action implements Behaviour {
 	
 	@Override
 	public Action getAction(Actor actor, GameMap map) {
-		if(actor instanceof Stegosaur) {
-			//if actor is a stegosaur 
-			//for (int i = 0 ; i < Application.map.size() ; i++);
+		Location currentPosition = map.locationOf(actor);
+		
+		//For Stegosaur
+		if((actor instanceof Stegosaur) && (isFruitAround(actor,map)|| isGrassAround(actor,map)) ) {
+			for(Exit exit: currentPosition.getExits()) {
+				Location destination = exit.getDestination() ; 
+				if(destination.canActorEnter(actor)) {
+					//Hunt for fruit
+					for(Item item: destination.getItems()) {
+						if(item instanceof Fruit) {
+							Fruit fruit = (Fruit) item ; 
+							map.moveActor(actor,destination);
+							Stegosaur stegosaur = (Stegosaur)actor ; 
+							stegosaur.eatFruit(fruit);
+							destination.removeItem(item);
+							return this ; 
+						}
+					}
+					//Hunt for grass
+					if(destination.getGround() instanceof Grass) {
+						Grass grass = (Grass) destination.getGround() ; 
+						map.moveActor(actor,destination);
+						Stegosaur stegosaur = (Stegosaur)actor ; 
+						stegosaur.grazeGrass(grass);
+						destination.setGround(new Dirt());
+						return this; 
+					}	
+				}
+			}
+		
+			
+			// For Allosaur	
+		}else if((actor instanceof Allosaur) && isDinosaurCorpseAround(actor,map)){
+			for(Exit exit: currentPosition.getExits()) {
+				Location destination = exit.getDestination() ; 
+				if(destination.canActorEnter(actor)) {
+					//Hunt for corpse
+					for(Item item: destination.getItems()) {
+						if(item instanceof Corpse) {
+							Corpse corpse = (Corpse) item ; 
+							map.moveActor(actor,destination);
+							Allosaur allosaur = (Allosaur)actor ; 
+							allosaur.eatCorpse(corpse);
+							destination.removeItem(item);
+							return this ; 
+						}
+					}
+					
+				
+				}
+			}
+			
 		}else {
-			//if actor is an allosaur
+			for(Exit exit: currentPosition.getExits()) {
+				Location destination = exit.getDestination() ; 
+				if(destination.canActorEnter(actor)) {
+					//just wander cause no foods around
+					map.moveActor(actor,destination);
+					return this ; 
+				}
+			}
+			
+			
 		}
-	return null ; 
+		return null ;  // if all place occupied and can't go 
+	 
 	}
 
 	@Override
